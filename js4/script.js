f98ecaff08afe16d60ac7ec65130d96a304b621a
kindrout <artik241999@gmail.com>
2022-04-29 12:50:40 +0300

    make hw js

--- js4/script.js
+++ js4/script.js
@@ -1,94 +1,73 @@
-function square(a,b,action) {
-    return a * b;
-}
-
-console.log(square(12,24));
-
-
-
-
-let mas = [
-    {name:'Artur', age: 32, status: 'true', mind: 'on'},
-    {name:'Andiy', age: 22, status: 'true', mind: 'off'},
-]
-
-function mass(array) {
-    for (let item of array) {
-        console.log(item);
-    }
-}
-
-mass(mas);
-
-
-function text (massage) {
-    document.write(`<p>${massage}</p>`);
-}
-
-text('Hello Guys');
-
-function list(text1, number) {
-   document.write(`<ol>`)
-    for (let i = 0; i < number; i++) {
-        document.write(`<li>${text1}</li>`)
-    }
-
-    }
-   document.write(`</ol>`)
-
-
-list('Hello Girls', 19);
-
-
-let num = [23,45,88,54,12,34]
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+// function square(a,b) {
+//     return a * b;
+// }
+// console.log(square(10,30));
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+// - створити функцію яка приймає масив та виводить кожен його елемент
+// let arr = [ {name: 'artur', age:22, habbit:'work'},
+//             {name: 'andrii', age:244, habbit:'notwork'},
+// ];
+// function users (arrey) {
+//     for (const argument of arrey) {
+//     }
+//     console.log(arr);
+// }
+// users(arr);
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+// function text(texts) {
+//     document.write(`<p>${texts}</p>`);
+// }
+// text('embrion');
+// // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// function list(text) {
+//     document.write(`<ul>`);
+//     for (let i = 0; i < 3; i++) {
+//         document.write(`<li>${text}</li>`);
+//     }
+//     document.write(`</ul>`);
+// }
+// list('LoL');
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+// - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+// - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+// let arr = [ {name: 'artur', age:22, habbit:'work'},
+//             {name: 'andrii', age:244, habbit:'notwork'},
+// ];
+// function user(array) {
+//     console.log(array);
+// }
+// user(arr);
+// - створити функцію яка повертає найменьше число з масиву
+// let numbers = [10,32,4,5,7,67,67,11];
 // function minCalc (array) {
-//    let minNum = array[0];
+//     let minNum = array[0];
 //     for (let i = 0; i < minNum; i++)
-//        if (minNum > array[i]) {
-//            minNum = array[i];
-//        }
-//
-//     console.log(minNum);
-//    }
-//
-// minCalc(num);
+//         if (minNum > array[i]) {
+//             minNum = array[i]
+//         }
+//             let result = minNum;
+//             console.log(result);
+//     }
+// minCalc(numbers);
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
 
-
-
-
-
-
-function minCalc (arr) {
-    let minNum = arr[0]
-    for (let i = 0; i < minNum; i++) {
-        const minNumElement = arr[i];
-        if (minNum > arr[i]) {
-            minNum = arr[i]
-            console.log(minNum);
-        }
-    }
-}
-
-minCalc (num);
-
-
-// function calc() {
+// let numbers = [10,32,4,5,7,67,67,11];
+// function plus() {
 //     let result = 0;
-//     for (let item of arguments) {
-//         result = result + item;
+//     for (const item of arguments) {
+//         result = item + result;
 //     }
 //     return result;
 // }
 //
-// console.log(calc(12,22,33,44));
+// console.log(plus(10,32,4,5,7,67,67,11));
+//
 
-function calc() {
-    let result = 0;
-    for (let item of arguments) {
-        result = item + result;
-    }
-    return result;
-}
 
-console.log(calc(12,23,55,63,78));
+
+
+
+
 
